57b27685af0567ed53192c8cae76bb9cb6e00562
fordat <fordatwater@gmail.com>
2022-05-24 16:02:31 -0700

    login

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,6 +12,17 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return <>
     <section>
       <h1>
@@ -21,8 +32,53 @@ function Register() {
     </section>
 
     <section className="form">
-      <form>
-        <input type="text" className="form-control" id="name" name="name" value={name} placeholder="Enter your name"/>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            className="form-control" 
+            id="name" 
+            name="name" 
+            value={name} 
+            placeholder="Enter your name"
+            onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            className="form-control" 
+            id="email" 
+            name="email" 
+            value={email} 
+            placeholder="Enter your email"
+            onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password" 
+            name="password" 
+            value={password} 
+            placeholder="Enter your password"
+            onChange={onChange}/>
+        </div>        
+        <div className="form-group">
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password2" 
+            name="password2" 
+            value={password2} 
+            placeholder="Confirm password"
+            onChange={onChange}/>
+        </div>  
+        <div className="form-group">
+          <button type='submit' className='btn btn-block'>
+            Submit
+          </button>
+        </div>
+
       </form>
     </section>
   </>
